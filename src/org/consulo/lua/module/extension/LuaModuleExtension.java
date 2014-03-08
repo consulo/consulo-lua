f1101a216840666497e362f543c2ea728c633ff4
VISTALL <vistall.valeriy@gmail.com>
2014-03-08 16:51:34 +0200

    compilation fixed

--- src/org/consulo/lua/module/extension/LuaModuleExtension.java
+++ src/org/consulo/lua/module/extension/LuaModuleExtension.java
@@ -1,22 +1,25 @@
 package org.consulo.lua.module.extension;
 
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.projectRoots.SdkType;
-import com.sylvanaar.idea.Lua.sdk.LuaSdkType;
 import org.consulo.module.extension.impl.ModuleExtensionWithSdkImpl;
 import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.projectRoots.SdkType;
+import com.intellij.openapi.roots.ModifiableRootModel;
+import com.sylvanaar.idea.Lua.sdk.LuaSdkType;
 
 /**
  * @author VISTALL
  * @since 12.07.13.
  */
-public class LuaModuleExtension extends ModuleExtensionWithSdkImpl<LuaModuleExtension> {
-	public LuaModuleExtension(@NotNull String id, @NotNull Module module) {
+public class LuaModuleExtension extends ModuleExtensionWithSdkImpl<LuaModuleExtension>
+{
+	public LuaModuleExtension(@NotNull String id, @NotNull ModifiableRootModel module)
+	{
 		super(id, module);
 	}
 
 	@Override
-	protected Class<? extends SdkType> getSdkTypeClass() {
+	protected Class<? extends SdkType> getSdkTypeClass()
+	{
 		return LuaSdkType.class;
 	}
 }
